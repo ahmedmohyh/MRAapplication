cb19548f37592343d3617d5b19495195c89b275b
x4ahmed <x4ahmed.mostafa@outlook.com>
2022-01-14 21:51:18 +0100

    New SQL script: edited the Rating table.

--- SQL/SQLForMra.sql
+++ SQL/SQLForMra.sql
@@ -1,4 +1,4 @@
-create database MRA; 
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -22,7 +22,7 @@ ALTER TABLE `Movie`
   CREATE TABLE `UserData` (
   `id` int(11) NOT NULL,
   `age` int(11) NOT NULL,
-  `userName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `userName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL UNIQUE,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL	
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -36,6 +36,7 @@ ALTER TABLE `UserData`
   `id` int(11) NOT NULL,
   `rating` int(11) NOT NULL,
   `FilmID` int(11) NOT NULL,
+  `userName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL UNIQUE,
   `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -43,7 +44,10 @@ ALTER TABLE `Rating`
  ADD PRIMARY KEY (`id`);
   
 ALTER TABLE `Rating`
-  ADD foreign key (`FilmID`) references Movie(`id`) ;
+  ADD foreign key (`FilmID`) references Movie(`id`);
+  
+ALTER TABLE `Rating`
+  ADD foreign key (`userName`) references UserData(`userName`);
   
 ALTER TABLE `Rating`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
