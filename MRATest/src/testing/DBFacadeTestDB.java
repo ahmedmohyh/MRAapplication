a995f962560c755ece83e7f33f9c54779859670b
Ahmed <amohy2015@gmail.com>
2022-01-20 01:08:42 +0100

    [MRATest] TEST Project added

---
+++ MRATest/src/testing/DBFacadeTestDB.java
@@ -0,0 +1,44 @@
+package testing;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import junit.framework.TestCase;
+
+/**
+ * Testing our DBFacade.
+ * 
+ * @author swe.uni-due.de
+ *
+ */
+public class DBFacadeTestDB extends TestCase {
+
+
+	/**
+	 * Preparing classes with static methods
+	 */
+
+
+	/**
+	 * Testing getAvailableHolidayOffers with non-empty results.
+	 */
+
+
+	/**
+	 * Testing getAvailableHolidayOffer with empty result.
+	 */
+
+
+	/**
+	 * Rest database
+	 */
+
+}
---
+++ MRATest/src/testing/MRAapplicationTest.java
@@ -0,0 +1,26 @@
+package testing;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import application.MRAapplication;
+import org.junit.Test;
+
+import dbadapter.DBFacade;
+import junit.framework.TestCase;
+
+public class MRAapplicationTest extends TestCase {
+	
+	public MRAapplicationTest() {
+		super();
+	}
+	
+	@Test
+	public void testByDefault() {
+		//DBFacade stub = mock(DBFacade.class);
+		//DBFacade.setInstance(stub);
+		System.out.println("TEST");
+	}
+
+}
